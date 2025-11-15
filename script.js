48355aaa89dfe3b6d4c06e522f5a86313d78e8b8
Cat-cat-cat-cat-boop <brrecks2013@gmail.com>
2025-11-15 04:40:55 +0000

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-var opperator = Math.round(Math.random()*4,1)+1
 function RandomCustom(min,max) {
     var RandomNumber = Math.floor(Math.random()*(max-min+1))+min;
     return RandomNumber;
 }
-var number1;
-var number2;
 function GetEquation() {
-/* getting the random values */
-if (opperator==1) { /* addition */
-    number1= RandomCustom(1,200); number1= number1.toString();
-    number2= RandomCustom(1,200); number2= number2.toString();
-    opperator= "+";
-} else if (opperator==2) { /* subtraction */
-    number1= RandomCustom(1,200); number1= number1.toString();
-    number2= RandomCustom(1,200); number2= number2.toString();
-    opperator= "-";
-} else if (opperator==3) { /* multiplication */
-    number1= RandomCustom(1,100); number1= number1.toString();
-    number2= RandomCustom(1,100); number2= number2.toString();
-    opperator= "*";
-} else if (opperator==4) { /* division */
-    number2= RandomCustom(1,10); number2= number2.toString();
-    number1= RandomCustom(1,50)*number2; number1= number1.toString();
-    opperator= "÷";
-} else if (opperator==5) { /* exponentiation */
-    number1= RandomCustom(1,10); number1= number1.toString();
-    number2= RandomCustom(0,3); number2= number2.toString();
-    opperator= "^";
-}
-var equation = number1+opperator+number2;
+    var number1;
+    var number2;
+    var opperator = Math.round(Math.random()*4,1)+1
+    /* getting the random values */
+    if (opperator==1) { /* addition */
+        number1= RandomCustom(1,200); number1= number1.toString();
+        number2= RandomCustom(1,200); number2= number2.toString();
+        opperator= "+";
+    } else if (opperator==2) { /* subtraction */
+        number1= RandomCustom(1,200); number1= number1.toString();
+        number2= RandomCustom(1,200); number2= number2.toString();
+        opperator= "-";
+    } else if (opperator==3) { /* multiplication */
+        number1= RandomCustom(1,100); number1= number1.toString();
+        number2= RandomCustom(1,100); number2= number2.toString();
+        opperator= "*";
+    } else if (opperator==4) { /* division */
+        number2= RandomCustom(1,10); number2= number2.toString();
+        number1= RandomCustom(1,50)*number2; number1= number1.toString();
+        opperator= "÷";
+    } else if (opperator==5) { /* exponentiation */
+        number1= RandomCustom(1,10); number1= number1.toString();
+        number2= RandomCustom(0,3); number2= number2.toString();
+        opperator= "^";
+    }
+    var equation = number1+opperator+number2;
 document.getElementById("display").textContent = equation;
 }
 GetEquation();
