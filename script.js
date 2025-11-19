41dbdce53f9bb991aa38a2630f6c79400a7fe17f
TheSecondComing123 <jiuprogrammer@gmail.com>
2025-11-19 16:01:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var opperator = "";
 var number1 = "";
 var number2 = "";
 var gameStarted = false;
+var level = 5;
+var complexEquation = "";
 
 function RandomCustom(min, max) {
     var RandomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -33,50 +35,106 @@ function StartGame() {
     var displayElement = document.getElementById("display");
     displayElement.style.color = "";
 
-    GetEquation();
+    MakeComplexEquation(level);
 }
 
-function GetEquation() {
+function GetEquation(min, max, opperator) {
     if (!gameStarted) return;
 
     var number1;
     var number2;
-    var opperator = Math.round(Math.random() * 4, 1) + 1
 
     document.getElementById("top-display").textContent = "";
 
     /* getting the random values */
     if (opperator == 1) { /* addition */
-        number1 = RandomCustom(1, 200); number1 = number1.toString();
-        number2 = RandomCustom(1, 200); number2 = number2.toString();
+        number1 = RandomCustom(min, max); number1 = number1.toString();
+        number2 = RandomCustom(min, max); number2 = number2.toString();
         opperator = "+";
     } else if (opperator == 2) { /* subtraction */
-        number1 = RandomCustom(1, 200); number1 = number1.toString();
-        number2 = RandomCustom(1, 200); number2 = number2.toString();
+        number1 = RandomCustom(min, max); number1 = number1.toString();
+        number2 = RandomCustom(min, max); number2 = number2.toString();
         opperator = "-";
     } else if (opperator == 3) { /* multiplication */
-        number1 = RandomCustom(1, 100); number1 = number1.toString();
-        number2 = RandomCustom(1, 100); number2 = number2.toString();
+        number1 = RandomCustom(min, max); number1 = number1.toString();
+        number2 = RandomCustom(min, max); number2 = number2.toString();
         opperator = "*";
     } else if (opperator == 4) { /* division */
-        number2 = RandomCustom(1, 10); number2 = number2.toString();
-        number1 = RandomCustom(1, 50) * number2; number1 = number1.toString();
+        number2 = RandomCustom(min, max); number2 = number2.toString();
+        number1 = RandomCustom(min, max) * number2; number1 = number1.toString();
         opperator = "/";
     } else if (opperator == 5) { /* exponentiation */
-        number1 = RandomCustom(1, 10); number1 = number1.toString();
-        number2 = RandomCustom(1, 3); number2 = number2.toString();
+        number1 = RandomCustom(min, max); number1 = number1.toString();
+        number2 = RandomCustom(min, max); number2 = number2.toString();
         opperator = "**";
     }
 
     equation = number1 + opperator + number2;
+}
 
-    console.log(equation);
+function MakeComplexEquation(level) {
+    var opperator;
+    var opperators = ["+", "-", "*", "/", "**"]
+    if (level == 1) {
+        opperator = Math.round(Math.random(), 1) + 1
+        GetEquation(1, 20)
+        complexEquation = equation
+    }
+    if (level == 2) {
+        opperator = Math.round(Math.random() * 3, 1) + 1
+        if (opperator == 1 || opperator == 2) { GetEquation(1, 100) }
+        else { GetEquation(1, 10) }
+        complexEquation = equation
+    }
+    if (level == 3) {
+        opperator = Math.round(Math.random() * 3, 1) + 1
+        if (opperator == 1 || opperator == 2) { GetEquation(-200, 200) }
+        else { GetEquation(-20, 20) }
+        complexEquation = equation
+    }
+    if (level == 4) {
+        opperator = Math.round(Math.random() * 4, 1) + 1
+        if (opperator == 1 || opperator == 2) { GetEquation(-500, 500) }
+        else if (opperator == 3) { GetEquation(-100, 100) }
+        else if (opperator == 4) { GetEquation(-50, 50) }
+        else { GetEquation(0, 5) }
+        complexEquation = equation
+    }
+    if (level == 5) {
+        opperator = Math.round(Math.random() * 4, 1) + 1 /* Getting the first part */
+        if (opperator == 1 || opperator == 2) { GetEquation(-500, 500) }
+        else if (opperator == 3) { GetEquation(-100, 100) }
+        else if (opperator == 4) { GetEquation(-50, 50) }
+        else { GetEquation(0, 5) }
+
+        equation = equation + opperators[Math.round(Math.random() * 4, 1)] + "("
+        complexEquation = equation /* Adding the () */
+
+        opperator = Math.round(Math.random() * 4, 1) + 1 /* The part inside the () */
+        if (opperator == 1 || opperator == 2) { GetEquation(-500, 500) }
+        else if (opperator == 3) { GetEquation(-100, 100) }
+        else if (opperator == 4) { GetEquation(-50, 50) }
+        else { GetEquation(0, 5) }
+
+        equation = equation + opperators[Math.round(Math.random() * 4, 1)] + ")"
+        complexEquation = complexEquation + equation /* Closing the () */
+
+        if (Math.round(Math.random()) == 1) { /* Adding optional ending portion */
+            opperator = Math.round(Math.random() * 4, 1) + 1
+            if (opperator == 1 || opperator == 2) { GetEquation(-500, 500) }
+            else if (opperator == 3) { GetEquation(-100, 100) }
+            else if (opperator == 4) { GetEquation(-50, 50) }
+            else { GetEquation(0, 5) }
+            complexEquation = complexEquation + equation
+        }
+    }
+    console.log(complexEquation);
 
     var displayElement = document.getElementById("display");
     displayElement.style.opacity = "0";
 
     setTimeout(function () {
-        displayElement.textContent = equation;
+        displayElement.textContent = complexEquation;
         displayElement.style.opacity = "1";
     }, 150);
 }
@@ -85,7 +143,7 @@ function AddNumber(Number) {
     if (!gameStarted) return;
 
     if (input === "") {
-        document.getElementById("top-display").textContent = equation;
+        document.getElementById("top-display").textContent = complexEquation;
     }
 
     input = input + Number;
@@ -102,7 +160,7 @@ function ClearInput() {
     if (!gameStarted) return;
     input = "";
     var displayElement = document.getElementById("display");
-    displayElement.textContent = equation;
+    displayElement.textContent = complexEquation;
     displayElement.style.color = "";
     document.getElementById("top-display").textContent = "";
 }
@@ -110,25 +168,25 @@ function ClearInput() {
 function NumberCheck() {
     if (!gameStarted) return;
 
-    console.log(equation)
+    console.log(complexEquation)
     var displayElement = document.getElementById("display");
-    if (input == eval(equation)) {
+    if (input == eval(complexEquation)) {
         displayElement.textContent = "Correct";
         displayElement.style.color = "lightgreen";
         input = "";
         setTimeout(function () {
             displayElement.style.color = "";
-            GetEquation();
+            MakeComplexEquation(level);
         }, 1000);
     } else if (displayElement.textContent == "Correct") {
-        GetEquation();
+        MakeComplexEquation(level);
     } else {
         displayElement.textContent = "Incorrect";
         displayElement.style.color = "firebrick";
         input = "";
         setTimeout(function () {
             displayElement.style.color = "";
-            GetEquation();
+            MakeComplexEquation(level);
         }, 1000);
     }
 }
